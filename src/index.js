5290fac1c630de41096359e52b3cb3cc7a5c9a31
shapirodaniel <shapirodanieladam@gmail.com>
2022-01-26 22:52:10 -0600

    add: lesson flow

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
+// 1st
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
 let numMoves = 0;
 
+// 2nd
 function App() {
+  // 3rd
   const squares = new Array(9).fill(null).map((_, idx) => idx);
 
+  // 4th, talk about declarative vs imperative
+  // how will we manipulate state?
   const handleMove = (e) => {
     if (e.target.innerText) return;
     e.target.innerText = numMoves % 2 ? 'O' : 'X';
     numMoves++;
   };
 
+  // 3rd
   return (
     <div className="board" onClick={handleMove}>
       {squares.map((val) => (
@@ -22,4 +28,5 @@ function App() {
   );
 }
 
+// 1st
 ReactDOM.render(<App />, document.getElementById('root'));
